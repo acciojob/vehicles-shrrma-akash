33a14d435c61a6861aa1792a4a2b11d15027152f
Akash Sharma <akash.bohareji@gmail.com>
2023-02-11 15:42:18 +0530

    Contest Assignment

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,6 +1,7 @@
 package com.driver;
 
 public class Car extends Vehicle {
+
     private int wheels;
     private String type;
     private int doors;
@@ -10,16 +11,52 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
+        this.wheels=wheels;
+        this.type=type;
+        this.doors=doors;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.currentGear=1;
+        this.seats=seats;
         //Hint: Car extends Vehicle
     }
 
-    public void changeGear(int newGear){
+    public int getWheels() {
+        return wheels;
+    }
 
+    public String getType() {
+        return type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void changeGear(int newGear){
+        this.currentGear=newGear ;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        super.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,6 +3,7 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,0,0,0,isManual,"a",0);
         //Use arbitrary values for parameters which are not mentioned
     }
 
@@ -17,9 +18,23 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        if(newSpeed>= 1 && newSpeed<=50)
+            this.changeGear(1);
+        else if(newSpeed>=51 && newSpeed<=100)
+            this.changeGear(2);
+        else if(newSpeed>=101 && newSpeed<=150)
+            this.changeGear(3);
+        else if(newSpeed>=151 && newSpeed<=200)
+            this.changeGear(4);
+        else if(newSpeed>=201 && newSpeed<=250)
+            this.changeGear(5);
+        else
+            this.changeGear(6);
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            this.changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
